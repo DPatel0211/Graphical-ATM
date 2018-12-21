329a62345eed770f29d1367bd60bbfdfda2077cd
Dev Patel <dpatel@ucvts.tec.nj.us>
2018-12-21 08:10:55 -0500

    check in #1

--- src/view/CreateView.java
+++ src/view/CreateView.java
@@ -1,11 +1,16 @@
 package view;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import controller.ViewManager;
 
@@ -13,6 +18,16 @@ import controller.ViewManager;
 public class CreateView extends JPanel implements ActionListener {
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
+	private JPasswordField pinField;
+	private JTextField dobField;
+	private JTextField phoneField;				
+	private JTextField fnameField;			
+	private JTextField lnameField;			
+	private JTextField addressField;		
+	private JTextField cityField;				
+	private JTextField stateField;			
+	private JTextField zipField;
+	
 	
 	/**
 	 * Constructs an instance (or object) of the CreateView class.
@@ -25,7 +40,7 @@ public class CreateView extends JPanel implements ActionListener {
 		
 		this.manager = manager;
 		initialize();
-	}
+	} 
 	
 	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
 	
@@ -34,6 +49,18 @@ public class CreateView extends JPanel implements ActionListener {
 	 */
 	
 	private void initialize() {
+		this.setLayout(null);
+		
+		EnterInfo();
+		initPinField();
+		initDobField();
+		initPhoneField();
+		initFnameField();
+		initLnameField();
+		initAddressField();
+		initCityField();
+		initStateField();
+		initZIPField();
 		
 		// TODO
 		//
@@ -69,6 +96,131 @@ public class CreateView extends JPanel implements ActionListener {
 	 * 
 	 * @param e
 	 */
+	
+	private void EnterInfo() {
+		JLabel label = new JLabel("Enter the following to create an account:", SwingConstants.LEFT);
+		label.setBounds(100, 50, 500, 35);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		this.add(label);
+	}
+	
+	private void initPinField() {
+		JLabel label = new JLabel("PIN", SwingConstants.RIGHT);
+		label.setBounds(100, 100, 95, 35);
+		label.setLabelFor(pinField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		pinField = new JPasswordField(20);
+		pinField.setBounds(205, 100, 200, 35);
+		
+		this.add(label);
+		this.add(pinField);
+	}
+	
+	private void initDobField() {
+		JLabel label = new JLabel("DoB", SwingConstants.RIGHT);
+		label.setBounds(100, 140, 95, 35);
+		label.setLabelFor(dobField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		dobField = new JTextField(40);
+		dobField.setBounds(205, 140, 200, 35);
+		
+		this.add(label);
+		this.add(dobField);
+	}
+	
+	private void initPhoneField() {
+		JLabel label = new JLabel("Phone #", SwingConstants.RIGHT);
+		label.setBounds(100, 180, 95, 35);
+		label.setLabelFor(phoneField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		phoneField = new JTextField(40);
+		phoneField.setBounds(205, 180, 200, 35);
+		
+		this.add(label);
+		this.add(phoneField);
+	}
+	
+	private void initFnameField() {
+		JLabel label = new JLabel("First Name", SwingConstants.RIGHT);
+		label.setBounds(100, 220, 95, 35);
+		label.setLabelFor(fnameField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		fnameField = new JTextField(40);
+		fnameField.setBounds(205, 220, 200, 35);
+		
+		this.add(label);
+		this.add(fnameField);
+	}
+	
+	private void initLnameField() {
+		JLabel label = new JLabel("Last Name", SwingConstants.RIGHT);
+		label.setBounds(100, 260, 95, 35);
+		label.setLabelFor(lnameField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		lnameField = new JTextField(40);
+		lnameField.setBounds(205, 260, 200, 35);
+		
+		this.add(label);
+		this.add(lnameField);
+	}
+	
+	private void initAddressField() {
+		JLabel label = new JLabel("Address", SwingConstants.RIGHT);
+		label.setBounds(100, 300, 95, 35);
+		label.setLabelFor(addressField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		addressField = new JTextField(40);
+		addressField.setBounds(205, 300, 200, 35);
+		
+		this.add(label);
+		this.add(addressField);
+	}
+	
+	private void initCityField() {
+		JLabel label = new JLabel("City", SwingConstants.RIGHT);
+		label.setBounds(100, 340, 95, 35);
+		label.setLabelFor(cityField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		cityField = new JTextField(40);
+		cityField.setBounds(205, 340, 200, 35);
+		
+		this.add(label);
+		this.add(cityField);
+	}
+	
+	private void initStateField() {
+		JLabel label = new JLabel("Address", SwingConstants.RIGHT);
+		label.setBounds(100, 380, 95, 35);
+		label.setLabelFor(stateField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		stateField = new JTextField(40);
+		stateField.setBounds(205, 380, 200, 35);
+		
+		this.add(label);
+		this.add(stateField);
+	}
+	
+	private void initZIPField() {
+		JLabel label = new JLabel("ZIP Code", SwingConstants.RIGHT);
+		label.setBounds(100, 420, 95, 35);
+		label.setLabelFor(zipField);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		zipField = new JTextField(40);
+		zipField.setBounds(205, 420, 200, 35);
+		
+		this.add(label);
+		this.add(zipField);
+	}
+
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
